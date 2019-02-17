a9824b7ddaa3d07eeea9f94e4c4ee0c3d81014e3
skall <blazekm1lan@seznam.cz>
2019-02-17 18:33:53 +0100

    H bridge with interrupts

--- main/main.ino
+++ main/main.ino
@@ -7,12 +7,20 @@
 #define pinTX 2
 #define pinRX 3
 #define pinPower 9
+#define pinMotorA 11
+#define pinMotorB 12
 #define simSpeed 9600
 #define optoPin 11
+#define STAY 0
+#define FORWARD 1
+#define BACKWARDS 2
 
 char buffer[512];
 char numberMemory[16];
 int sendRun = 0;
+int runSeconds = 0;
+int timeLimit = 300;
+int currentDir = STAY;
 
 GPRS sim900(pinTX, pinRX, simSpeed);
 
@@ -36,22 +44,73 @@ void sendCmd(String s) {
   Serial.println(buffer);
 }
 
+ISR(TIMER1_OVF_vect)         // timer compare interrupt service routine
+{
+  TCNT1 = 65536;            // preload timer
+  if (sendRun == 0) {
+    digitalWrite(pinMotorA, LOW);
+    digitalWrite(pinMotorB, LOW);
+    runSeconds = 0;
+  } else {
+    int prob = !currentDir ? 40 : 70;
+    if (random(100) > prob) {
+      // change direction
+      int prevDir = currentDir;
+      while (prevDir == currentDir) {
+        currentDir = random(BACKWARDS + 1);
+      }
+    }
+    
+    runSeconds += 1;
+
+    if (currentDir == STAY) {
+      digitalWrite(pinMotorA, LOW);
+      digitalWrite(pinMotorB, LOW);
+    } else if (currentDir == FORWARD) {
+      digitalWrite(pinMotorA, HIGH);
+      digitalWrite(pinMotorB, LOW);
+    } else {
+      digitalWrite(pinMotorA, LOW);
+      digitalWrite(pinMotorB, HIGH);
+    }
+
+    if (runSeconds >= timeLimit) {
+      Serial.println("Run rutine finished");
+      sendRun = 0;
+    }
+  }
+}
+
 void setup() {
+  noInterrupts();
+  TCCR1A = 0;
+  TCCR1B = 0;
+
+  TCNT1 = 65536;            // preload timer 65536-16MHz/256/2Hz
+  TCCR1B |= (1 << CS12);    // 256 prescaler 
+  TIMSK1 |= (1 << TOIE1);   // enable timer overflow interrupt
+  interrupts();
+  
+  pinMode(pinPower, OUTPUT);
   pinMode(optoPin, OUTPUT);
+  pinMode(pinMotorA, OUTPUT);
+  pinMode(pinMotorB, OUTPUT);
+  
   Serial.begin(9600);
   
   Serial.println("Booting up...");
   Serial.println("Power up...");
-  sim900.powerUpDown(pinPower);
   
   while(!sim900.init()) {
       Serial.println("Error!");
-      delay(1000);
+      sim900.powerUpDown(pinPower);
   }
   
   delay(2000);
   Serial.println("Boot done!");
   sendCmd("AT+COLP=1\r\n");
+  sendCmd("AT+COPS\r\n");
+  sendCmd("AT+CSQ\r\n");
   delay(1000);
 }
 
@@ -65,21 +124,11 @@ void loop() {
     sim900.hangup();
     Serial.println(numberMemory);
 
-    if (strcmp(numberMemory, "+420*********") == 0) {
+    if (strcmp(numberMemory, "+420***") == 0) {
       if (!sendRun) {
         sendRun = 1;
         Serial.println("Run rutine started");
       }
     }
-  }
-
-  if (sendRun) {
-    Serial.println("Power up toy..");
-    digitalWrite(optoPin, HIGH);
-    delay(100);
-    digitalWrite(optoPin, LOW);
-    delay(10000);
-    sendRun = 0;
-    sendCmd("AT+COLP=1\r\n");
   }
 }
